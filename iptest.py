fdbd749de36ea81c917f85a3aac98e63a085bb21
zhaoyihao <zhaoyihao@DESKTOP-DKFQFBG>
2017-06-01 10:58:05 +0800

    代理ip使用错误  更改

--- iptest.py
+++ iptest.py
@@ -1,33 +1,20 @@
-# -*- coding: utf-8 -*-
-
-import urllib
+# coding=utf-8
 import urllib2
-import re
-import urllib2
-import urllib
-import time
-import socket
-import random
-# 特稳定IP 107.151.152.218:80
-# http://www.xicidaili.com/wn/测试可用IP '107.151.136.202:80',222.124.130.34:8080,'103.14.196.74:8080','107.151.142.114:80''54.169.238.128:9999'
-#agent = ['49.113.101.167:8090','113.78.28.205:8090','219.136.31.16:8090','119.131.83.227:8090','221.221.206.208:8090','116.52.16.57:8090','182.109.80.149:9000','59.55.59.41:9000','115.223.201.206:9000']
-proxy_support = urllib2.ProxyHandler({'http': '218.18.232.29:8080'})
+import traceback
 
+# 西刺代理 http://www.xicidaili.com/nn/
+# 检查代理http://ip.chinaz.com/getip.aspx
+proxy_support = urllib2.ProxyHandler({'http': '220.166.96.90:82'})
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3107.4 Safari/537.36'
+}
 opener = urllib2.build_opener(proxy_support)
-urllib2.install_opener(opener)
 try:
-    response = urllib2.urlopen('http://zhao.zhixinst.com/Default.aspx')
-
+    request = urllib2.Request(
+        'http://ip.chinaz.com/getip.aspx', headers=headers)
+    response = opener.open(request)
     print(response.read())
-except Exception, e:
-    print e
+except:
+    print traceback.format_exc()
 
 print('*' * 50)
-exit(0)
-import urllib2
-import socks
-from sockshandler import SocksiPyHandler
-
-opener = urllib2.build_opener(
-    SocksiPyHandler(socks.SOCKS5, "116.226.90.12", 808))
-print urllib2.urlopen("http://zhao.zhixinst.com/Default.aspx").read()
